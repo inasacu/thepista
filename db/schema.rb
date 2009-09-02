d1efe7448a1b39bff81deb6062247b12be4b604e
Raul Padilla <raulpadilla@haypista.com>
2009-09-03 00:29:41 +0200

    set ignore these files

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091231190225) do
+ActiveRecord::Schema.define(:version => 20090927190225) do
 
   create_table "blogs", :force => true do |t|
     t.string   "name"
