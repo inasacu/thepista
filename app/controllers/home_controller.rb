b3d0ee445fff84d2d37229768a9ba52c00b1d6b0
Raul Padilla <raulpadilla@haypista.com>
2009-09-02 21:09:04 +0200

    simple changes to upcoming events and allow first direct show of events...

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,26 @@
 class HomeController < ApplicationController  
-   before_filter :require_user, :except => [:index, :about, :help, :welcome, :pricing]
+  before_filter :require_user, :except => [:index, :about, :help, :welcome, :pricing]
 
   def index
-    
+
     if current_user
       # @feed = current_user.activities
       # @users = current_user.find_mates
-      
-       @upcoming_schedules ||= Schedule.upcoming_schedules(session[:schedule_hide_time])
-      redirect_to :upcoming_schedule unless @upcoming_schedules.empty? 
-      return
+
+      if current_user.current_login_at >= (Time.now - 1.minutes)
+        @upcoming_schedules ||= Schedule.upcoming_schedules(session[:schedule_hide_time])
+        redirect_to :upcoming_schedule unless @upcoming_schedules.empty? 
+        return
+      end
     end    
     respond_to do |format|
       format.html
       format.atom
     end  
   end
-  
+
+  def upcoming_schedule
+    @upcoming_schedules ||= Schedule.upcoming_schedules(session[:schedule_hide_time])
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_filter :require_user, :only => [:index, :show, :edit, :update] 
   
   before_filter :get_user, :only => [:show, :set_available, :set_private_phone, :set_private_profile, :set_enable_comments, 
-                                 :set_teammate_notification, :set_message_notification, :set_comment_notification] 
+                                     :set_teammate_notification, :set_message_notification, :set_comment_notification] 
   before_filter :get_user_group, :only =>[:set_sub_manager, :remove_sub_manager, :set_subscription, 
                                           :remove_subscription, :set_moderator, :remove_moderator]
   
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def index
     store_location
     @users = current_user.page_mates(params[:page])
-  
+
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @users }
@@ -27,10 +27,10 @@ class UsersController < ApplicationController
     # @user = User.find(params[:id])
 
     # if @user.my_members?(current_user)
-      @previous = User.previous(@user, current_user.groups)
-      @next = User.next(@user, current_user.groups)
+    @previous = User.previous(@user, current_user.groups)
+    @next = User.next(@user, current_user.groups)
     # end
-    
+
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @user }
@@ -101,7 +101,7 @@ class UsersController < ApplicationController
       redirect_back_or_default('/index')
       return
     end
-    
+
     unless current_user.requested_managers.empty?
       @requested_teammates = Teammate.find(:all, :conditions => ["manager_id = ? and status = 'pending'", current_user.id])
     end
@@ -111,154 +111,152 @@ class UsersController < ApplicationController
   end
 
 
+  def search
+    count = User.count_by_solr(params[:search])
+    @users = User.paginate_all_by_solr(params[:search], :page => params[:page], :total_entries => count, :limit => 25, :offset => 1)
 
-    
-#     def search
-#       count = User.count_by_solr(params[:search])
-#       @users = User.paginate_all_by_solr(params[:search], :page => params[:page], :total_entries => count, :limit => 25, :offset => 1)
-# 
-#       # @non_member = true
-#       # @users = User.paginate_all_by_solr(params[:search].to_s, :page => params[:page])
-#       render :template => '/users/index'
-#     end
+    # @non_member = true
+    # @users = User.paginate_all_by_solr(params[:search].to_s, :page => params[:page])
+    render :template => '/users/index'
+  end
 
-    def set_sub_manager 
-      unless current_user.is_manager_of?(@group)
-        flash[:notice] = I18n.t(:unauthorized)  
-        return
-      end
-      @user.has_role!(:sub_manager, @group)
-      flash[:notice] = I18n.t(:sub_manager_updated)
-      redirect_back_or_default('/index')
+  def set_sub_manager 
+    unless current_user.is_manager_of?(@group)
+      flash[:notice] = I18n.t(:unauthorized)  
+      return
     end
+    @user.has_role!(:sub_manager, @group)
+    flash[:notice] = I18n.t(:sub_manager_updated)
+    redirect_back_or_default('/index')
+  end
 
-    def remove_sub_manager 
-      unless current_user.is_manager_of?(@group)
-        flash[:notice] = I18n.t(:unauthorized)  
-        return
-      end
-      @user.has_no_role!(:sub_manager, @group)
-      flash[:notice] = I18n.t(:sub_manager_updated)
-      redirect_back_or_default('/index')
+  def remove_sub_manager 
+    unless current_user.is_manager_of?(@group)
+      flash[:notice] = I18n.t(:unauthorized)  
+      return
     end
+    @user.has_no_role!(:sub_manager, @group)
+    flash[:notice] = I18n.t(:sub_manager_updated)
+    redirect_back_or_default('/index')
+  end
 
-    def set_subscription 
-      unless current_user.is_manager_of?(@group)
-        flash[:notice] = I18n.t(:unauthorized)  
-        return
-      end
-      @user.has_role!(:subscription, @group)
-      flash[:notice] = I18n.t(:subscription_updated)
-      redirect_back_or_default('/index')
-    end 
-    
-    def remove_subscription 
-      unless current_user.is_manager_of?(@group)
-        flash[:notice] = I18n.t(:unauthorized)  
-        return
-      end
-      @user.has_no_role!(:subscription, @group)
-      flash[:notice] = I18n.t(:subscription_updated)
-      redirect_back_or_default('/index')
+  def set_subscription 
+    unless current_user.is_manager_of?(@group)
+      flash[:notice] = I18n.t(:unauthorized)  
+      return
     end
-    
-    def set_moderator 
-      unless current_user.is_manager_of?(@group)
-        flash[:notice] = I18n.t(:unauthorized)  
-        return
-      end
-      @user.has_role!(:moderator, @group)
-      flash[:notice] = I18n.t(:moderator_updated)
+    @user.has_role!(:subscription, @group)
+    flash[:notice] = I18n.t(:subscription_updated)
+    redirect_back_or_default('/index')
+  end 
+
+  def remove_subscription 
+    unless current_user.is_manager_of?(@group)
+      flash[:notice] = I18n.t(:unauthorized)  
+      return
+    end
+    @user.has_no_role!(:subscription, @group)
+    flash[:notice] = I18n.t(:subscription_updated)
+    redirect_back_or_default('/index')
+  end
+
+  def set_moderator 
+    unless current_user.is_manager_of?(@group)
+      flash[:notice] = I18n.t(:unauthorized)  
+      return
+    end
+    @user.has_role!(:moderator, @group)
+    flash[:notice] = I18n.t(:moderator_updated)
+    redirect_back_or_default('/index')
+  end 
+
+  def remove_moderator 
+    unless current_user.is_manager_of?(@group)
+      flash[:notice] = I18n.t(:unauthorized)  
+      return
+    end
+    @user.has_no_role!(:moderator, @group)
+    flash[:notice] = I18n.t(:moderator_updated)
+    redirect_back_or_default('/index')
+  end
+
+  def set_available
+    if @user.update_attribute("available", !@user.available)
+      @user.update_attribute("available", @user.available)  
+
+      flash[:notice] = I18n.t(:successful_update)
       redirect_back_or_default('/index')
-    end 
-    
-    def remove_moderator 
-      unless current_user.is_manager_of?(@group)
-        flash[:notice] = I18n.t(:unauthorized)  
-        return
-      end
-      @user.has_no_role!(:moderator, @group)
-      flash[:notice] = I18n.t(:moderator_updated)
+    else
+      render :action => 'index'
+    end
+  end
+
+  def set_private_phone
+    if @user.update_attribute("private_phone", !@user.private_phone)
+      @user.update_attribute("private_phone", @user.private_phone)  
+
+      flash[:notice] = I18n.t(:successful_update)
       redirect_back_or_default('/index')
+    else
+      render :action => 'index'
     end
+  end
 
-    def set_available
-      if @user.update_attribute("available", !@user.available)
-        @user.update_attribute("available", @user.available)  
+  def set_private_profile
+    if @user.update_attribute("private_profile", !@user.private_profile)
+      @user.update_attribute("private_profile", @user.private_profile)  
 
-        flash[:notice] = I18n.t(:successful_update)
-        redirect_back_or_default('/index')
-      else
-        render :action => 'index'
-      end
+      flash[:notice] = I18n.t(:successful_update)
+      redirect_back_or_default('/index')
+    else
+      render :action => 'index'
     end
+  end  
 
-    def set_private_phone
-      if @user.update_attribute("private_phone", !@user.private_phone)
-        @user.update_attribute("private_phone", @user.private_phone)  
+  def set_enable_comments
+    if @user.update_attribute("enable_comments", !@user.enable_comments)
+      @user.update_attribute("enable_comments", @user.enable_comments)  
 
-        flash[:notice] = I18n.t(:successful_update)
-        redirect_back_or_default('/index')
-      else
-        render :action => 'index'
-      end
+      flash[:notice] = I18n.t(:successful_update)
+      redirect_back_or_default('/index')
+    else
+      render :action => 'index'
     end
-    
-    def set_private_profile
-      if @user.update_attribute("private_profile", !@user.private_profile)
-        @user.update_attribute("private_profile", @user.private_profile)  
+  end  
 
-        flash[:notice] = I18n.t(:successful_update)
-        redirect_back_or_default('/index')
-      else
-        render :action => 'index'
-      end
-    end  
+  def set_teammate_notification
+    if @user.update_attribute("teammate_notification", !@user.teammate_notification)
+      @user.update_attribute("teammate_notification", @user.teammate_notification)  
 
-    def set_enable_comments
-      if @user.update_attribute("enable_comments", !@user.enable_comments)
-        @user.update_attribute("enable_comments", @user.enable_comments)  
+      flash[:notice] = I18n.t(:successful_update)
+      redirect_back_or_default('/index')
+    else
+      render :action => 'index'
+    end
+  end  
 
-        flash[:notice] = I18n.t(:successful_update)
-        redirect_back_or_default('/index')
-      else
-        render :action => 'index'
-      end
-    end  
+  def set_message_notification
+    if @user.update_attribute("message_notification", !@user.message_notification)
+      @user.update_attribute("message_notification", @user.message_notification)  
 
-    def set_teammate_notification
-      if @user.update_attribute("teammate_notification", !@user.teammate_notification)
-        @user.update_attribute("teammate_notification", @user.teammate_notification)  
+      flash[:notice] = I18n.t(:successful_update)
+      redirect_back_or_default('/index')
+    else
+      render :action => 'index'
+    end
+  end  
 
-        flash[:notice] = I18n.t(:successful_update)
-        redirect_back_or_default('/index')
-      else
-        render :action => 'index'
-      end
-    end  
+  def set_comment_notification
+    if @user.update_attribute("blog_comment_notification", !@user.blog_comment_notification)
+      @user.update_attribute("blog_comment_notification", @user.blog_comment_notification)  
 
-    def set_message_notification
-      if @user.update_attribute("message_notification", !@user.message_notification)
-        @user.update_attribute("message_notification", @user.message_notification)  
+      flash[:notice] = I18n.t(:successful_update)
+      redirect_back_or_default('/index')
+    else
+      render :action => 'index'
+    end
+  end  
 
-        flash[:notice] = I18n.t(:successful_update)
-        redirect_back_or_default('/index')
-      else
-        render :action => 'index'
-      end
-    end  
-
-    def set_comment_notification
-      if @user.update_attribute("blog_comment_notification", !@user.blog_comment_notification)
-        @user.update_attribute("blog_comment_notification", @user.blog_comment_notification)  
-
-        flash[:notice] = I18n.t(:successful_update)
-        redirect_back_or_default('/index')
-      else
-        render :action => 'index'
-      end
-    end  
-    
     
 private
   def get_user
