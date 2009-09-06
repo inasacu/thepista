4bb8e404c65cb05acb0a6b9f283e223bb39692a6
Raul Padilla <raulpadilla@haypista.com>
2009-09-06 19:13:54 +0200

    issues in environment file and rpxnow

--- config/environment.rb
+++ config/environment.rb
@@ -75,7 +75,7 @@ end
 raw_config = File.read(RAILS_ROOT + "/config/config.yml")
 APP_CONFIG = YAML.load(raw_config)[RAILS_ENV]
 
-RPXNow.api_key = APP_CONFIG['rpx_api']['key']
+# RPXNow.api_key = APP_CONFIG['rpx_api']['key']
 
 
 
