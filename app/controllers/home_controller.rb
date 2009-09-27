cf128a897dcbed33b9203b568f3ed460cfd81230
Raul Padilla <raulpadilla@haypista.com>
2009-09-28 00:33:55 +0200

    fix when using upcoming events and activities

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,13 @@
 class HomeController < ApplicationController  
   before_filter :require_user, :except => [:index, :about, :help, :welcome, :pricing]
 
+  before_filter :get_user_mates
+
   def index
 
     if current_user
       # @feed = current_user.activities
-      @users = current_user.find_mates
+      # @users = current_user.find_mates
 
       if current_user.current_login_at >= (Time.now - 1.minutes)
         @upcoming_schedules ||= Schedule.upcoming_schedules(session[:schedule_hide_time])
@@ -21,6 +23,11 @@ class HomeController < ApplicationController
 
   def upcoming_schedule
     @upcoming_schedules ||= Schedule.upcoming_schedules(session[:schedule_hide_time])
+  end
+
+  private
+  def get_user_mates
+    @users = current_user.find_mates
   end
 
 end
