344309f1f8548f916f8402912e4f5f368dd303dc
Raul Padilla <raulpadilla@haypista.com>
2009-10-12 01:50:02 +0200

    working on timezone

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -50,16 +50,17 @@ class Schedule < ActiveRecord::Base
   belongs_to :marker
   belongs_to :invite_group,   :class_name => "Group",   :foreign_key => "invite_id"
    
-   
-   validates_presence_of       :concept,          :within => NAME_RANGE_LENGTH
-   validates_presence_of       :description,      :within => DESCRIPTION_RANGE_LENGTH
-   validates_presence_of       :starts_at
-   validates_presence_of       :time_zone
-   validates_presence_of       :sport_id
-   validates_presence_of       :marker_id
-   validates_presence_of       :group_id
-   validates_numericality_of   :fee_per_game
-   validates_numericality_of   :fee_per_pista
+  # validations   
+  validates_presence_of       :concept,          :within => NAME_RANGE_LENGTH
+  validates_presence_of       :description,      :within => DESCRIPTION_RANGE_LENGTH
+  validates_presence_of       :starts_at
+  validates_presence_of       :ends_at
+  validates_presence_of       :time_zone
+  validates_presence_of       :sport_id
+  validates_presence_of       :marker_id
+  validates_presence_of       :group_id
+  validates_numericality_of   :fee_per_game
+  validates_numericality_of   :fee_per_pista
 
   # variables to access
   attr_accessible :concept, :season, :jornada, :starts_at, :ends_at, :reminder, :subscription_at, :non_subscription_at
@@ -68,6 +69,7 @@ class Schedule < ActiveRecord::Base
   
   
   # after_update        :save_matches
+  before_update       :set_time_to_utc
   after_create        :log_activity
   after_update        :log_activity_played
 
@@ -197,6 +199,11 @@ class Schedule < ActiveRecord::Base
   end
   
   private
+  def set_time_to_utc
+    self.starts_at = self.starts_at.utc
+    self.ends_at = self.ends_at.utc
+  end
+  
   def log_activity
     add_activities(:item => self, :user => self.group.all_the_managers.first) 
   end
