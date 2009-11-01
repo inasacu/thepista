8fa4cb2355a36e79dc21cd1b206ddaeea7d569e0
Raúl Padilla <inasacu@gmail.com>
2009-11-01 01:36:27 +0100

    id issues on groups_users table

--- app/models/groups_markers.rb
+++ app/models/groups_markers.rb
@@ -1,7 +1,10 @@
 class GroupsMarkers < ActiveRecord::Base
   
-  set_primary_key :group_id
-  set_primary_key :marker_id
+  # set_primary_key :group_id
+  # set_primary_key :marker_id
+  
+  # variables to access
+  attr_accessible :group_id, :marker_id
   
   # record a marker join
   def self.join_marker(group, marker)
