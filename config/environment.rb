33fa6788b8c47f3c821d4612e6a0edccdaebdd71
Raúl Padilla <inasacu@gmail.com>
2009-12-02 23:49:46 +0100

    added new relic to application for monitoring performance

--- config/environment.rb
+++ config/environment.rb
@@ -31,7 +31,8 @@ Rails::Initializer.run do |config|
   config.gem "contacts"  
   config.gem "friendly_id"
   config.gem 'sitemap_generator', :lib => false, :source => 'http://gemcutter.org'
-  
+  config.gem "newrelic_rpm" 
+    
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
