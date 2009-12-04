24fecd882ad626c6fcf8cd44fafffb975bad53e0
Raúl Padilla <inasacu@gmail.com>
2009-12-04 02:09:04 +0100

    remove newrelic_rmp from environment

--- config/environment.rb
+++ config/environment.rb
@@ -31,7 +31,7 @@ Rails::Initializer.run do |config|
   config.gem "contacts"  
   config.gem "friendly_id"
   config.gem 'sitemap_generator', :lib => false, :source => 'http://gemcutter.org'
-  config.gem "newrelic_rpm" 
+  # config.gem "newrelic_rpm" 
     
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
