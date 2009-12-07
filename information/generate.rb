a6ede642bcae63d2ad856abf62b8b781af992b15
Raúl Padilla <inasacu@gmail.com>
2009-12-07 20:58:48 +0100

    removed info files and change to tems of use file

--- information/generate.rb
+++ information/generate.rb
@@ -8,3 +8,10 @@ ruby script/generate themed tournaments tournament --layout=application --with_w
 
 ruby script/generate nifty_scaffold feeds
 
+
+
+
+ruby script/generate controller rounds
+ruby script/generate model round
+rake db:migrate
+ruby script/generate themed rounds round --layout=application --with_will_paginate
