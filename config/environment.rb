88b965896df0a94669b0df0e1061f3fb66fa712f
Raúl Padilla <inasacu@gmail.com>
2010-02-01 21:19:01 +0100

    removed plugin hoptoad and replaced w/ gem

--- config/environment.rb
+++ config/environment.rb
@@ -35,12 +35,11 @@ Rails::Initializer.run do |config|
   config.gem "contacts"  
   config.gem "friendly_id"
   config.gem 'sitemap_generator', :lib => false, :source => 'http://gemcutter.org'
-  # config.gem "newrelic_rpm" 
   config.gem "edgarjs-ajaxful_rating", :lib => "ajaxful_rating", :source => "http://gems.github.com"
   config.gem 'jackdempsey-acts_as_commentable', :lib => 'acts_as_commentable', :source => "http://gems.github.com"
   config.gem "acts-as-taggable-on", :source => "http://gemcutter.org"
-  
-  
+  config.gem 'hoptoad_notifier'
+    
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,3 +1,5 @@
 HoptoadNotifier.configure do |config|
   config.api_key = 'b63604603b22867dddb914f893ae2104'
 end
+
+# ruby script/generate hoptoad --api-key b63604603b22867dddb914f893ae2104
