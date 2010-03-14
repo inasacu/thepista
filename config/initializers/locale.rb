297a04829813b1843f597b891c32e401d8b0a1e9
Raúl Padilla <inasacu@gmail.com>
2010-03-14 14:27:40 +0100

    removed dup init of language and added looking fields

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -6,4 +6,4 @@ I18n.load_path << Dir[ File.join(RAILS_ROOT, 'lib', 'locales', '*.{rb,yml}') ]
 I18n.default_locale = :es
 
 # languages currently supported
-LANGUAGES = ['en', 'es']
+# LANGUAGES = ['en', 'es']
---
+++ db/migrate/20100314131016_add_group_user_looking.rb
@@ -0,0 +1,13 @@
+class AddGroupUserLooking < ActiveRecord::Migration
+  def self.up
+    # add_column      :schedules,     :reminder_at,               :datetime
+    # add_column      :groups,        :looking,          :boolean,         :default => true
+    # add_column      :users,         :looking,         :boolean,         :default => true
+  end
+
+  def self.down
+    remove_column     :schedules,     :reminder_at
+    remove_column     :groups,        :looking
+    remove_column     :users,         :looking
+  end
+end
