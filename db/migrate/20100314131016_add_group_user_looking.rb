ecacf83fcd12341313fb552d89c0706c56dc6390
Raúl Padilla <inasacu@gmail.com>
2010-03-14 14:29:03 +0100

    uncomment new fields

--- db/migrate/20100314131016_add_group_user_looking.rb
+++ db/migrate/20100314131016_add_group_user_looking.rb
@@ -1,8 +1,8 @@
 class AddGroupUserLooking < ActiveRecord::Migration
   def self.up
-    # add_column      :schedules,     :reminder_at,               :datetime
-    # add_column      :groups,        :looking,          :boolean,         :default => true
-    # add_column      :users,         :looking,         :boolean,         :default => true
+    add_column      :schedules,     :reminder_at,               :datetime
+    add_column      :groups,        :looking,          :boolean,         :default => true
+    add_column      :users,         :looking,         :boolean,         :default => true
   end
 
   def self.down
