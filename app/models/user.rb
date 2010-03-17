8f423ebb28ddf13c964c391035a9a0a66c8b0778
Raúl Padilla <inasacu@gmail.com>
2010-03-17 01:48:15 +0100

    removed LANGUAGES and hard coded array of languages in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,18 +24,9 @@ class User < ActiveRecord::Base
   validates_inclusion_of   :language,     :in => ['en', 'es'],    :allow_nil => false
   
   # validates_format_of   :name,            :with =>  /^[A-Z a-z 0-9]*\z/
-  
-  # RE_EMAIL_NAME   = '[\w\.%\+\-]+'                          # what you actually see in practice
-  # #RE_EMAIL_NAME   = '0-9A-Z!#\$%\&\'\*\+_/=\?^\-`\{|\}~\.' # technically allowed by RFC-2822
-  # RE_DOMAIN_HEAD  = '(?:[A-Z0-9\-]+\.)+'
-  # RE_DOMAIN_TLD   = '(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)'
-  # RE_EMAIL_OK     = /\A#{RE_EMAIL_NAME}@#{RE_DOMAIN_HEAD}#{RE_DOMAIN_TLD}\z/i
-  
-  
   # validates_acceptance_of :terms_of_service
   # validates_inclusion_of :gender, :in => ['male','female'], :allow_nil => true
  
-  # after_update      :log_activity_description_changed
   before_destroy    :destroy_activities, :destroy_feeds  
   before_destroy    :unmap_rpx
   
