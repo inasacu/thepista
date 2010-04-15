fb8eac8b7f076b5695ee407ba037a024bdd7ceae
Raúl Padilla <inasacu@gmail.com>
2010-04-15 21:01:22 +0200

    work started on cup

--- app/controllers/cups_controller.rb
+++ app/controllers/cups_controller.rb
@@ -1,2 +1,130 @@
 class CupsController < ApplicationController
+  before_filter :require_user    
+  before_filter :get_cup, :only => [:team_list, :show, :edit, :update, :set_available, :set_enable_comments, :set_looking, :destroy]
+  before_filter :has_manager_access, :only => [:edit, :update, :destroy, :set_available, :set_enable_comments, :set_looking]
+
+  def index
+    @cups = Cup.paginate :page => params[:page], :order => 'name' 
+
+    # if @cups.nil? or @cups.blank?
+    #   redirect_to :action => 'list'
+    #   return
+    # end
+  end
+
+  # def list
+  #   @cups = Cup.paginate(:all, :conditions => ["archive = false and id not in (?)", current_user.cups], 
+  #   :page => params[:page], :order => 'name') unless current_user.cups.blank?
+  #   @cups = Cup.paginate(:all, :conditions =>["archive = false"], 
+  #   :page => params[:page], :order => 'name') if current_user.cups.blank?
+  #   render :template => '/cups/index'       
+  # end
+
+  def team_list
+    @users = @cup.users.paginate(:page => params[:page], :per_page => USERS_PER_PAGE)
+    @total = @cup.users.count
+  end
+
+  def show
+    store_location 
+    # @cup = Cup.find(params[:id])
+  end
+
+  def new
+    @cup = Cup.new
+    @cup.time_zone = current_user.time_zone if !current_user.time_zone.nil?
+    @markers = Marker.find(:all)
+    @sports = Sport.find(:all)
+  end
+
+  def create
+    @cup = Cup.new(params[:cup])		
+    @user = current_user
+
+    if @cup.save and @cup.create_cup_details(current_user)
+      flash[:notice] = I18n.t(:successful_create)
+      redirect_to @cup
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    # @cup = Cup.find(params[:id])
+  end
+
+  def update
+    @original_cup = Cup.find(params[:id])
+
+    if @cup.update_attributes(params[:cup]) 
+      if (@original_cup.points_for_win != @cup.points_for_win) or 
+        (@original_cup.points_for_lose != @cup.points_for_lose) or 
+        (@original_cup.points_for_draw != @cup.points_for_draw)
+
+        Scorecard.send_later(:calculate_cup_scorecard, @cup)    
+      end
+
+      flash[:notice] = I18n.t(:successful_update)
+      redirect_to @cup
+    else
+      render :action => 'edit'
+    end
+  end 
+
+  def set_looking
+    if @cup.update_attribute("looking", !@cup.looking)
+      @cup.update_attribute("looking", @cup.looking)  
+
+      flash[:notice] = I18n.t(:successful_update)
+      redirect_back_or_default('/index')
+    else
+      render :action => 'index'
+    end
+  end   
+
+  def set_available
+    if @cup.update_attribute("available", !@cup.available)
+      @cup.update_attribute("available", @cup.available)  
+
+      flash[:notice] = I18n.t(:successful_update)
+      redirect_back_or_default('/index')
+    else
+      render :action => 'index'
+    end
+  end
+
+  def set_enable_comments
+    if @cup.update_attribute("enable_comments", !@cup.enable_comments)
+      @cup.update_attribute("enable_comments", @cup.enable_comments)  
+
+      flash[:notice] = I18n.t(:successful_update)
+      redirect_back_or_default('/index')
+    else
+      render :action => 'index'
+    end
+  end
+
+  def destroy
+    # @cup = Cup.find(params[:id])
+    counter = 0
+    @cup.schedules.each {|schedule| counter += 1 }
+
+    # @cup.destroy unless counter > 0
+
+    flash[:notice] = I18n.t(:successfully_destroyed)
+    redirect_to cup_url
+  end
+
+  private
+  def get_cup
+    @cup = Cup.find(params[:id])
+  end
+
+  def has_manager_access
+    unless current_user.is_manager_of?(@cup)
+      flash[:warning] = I18n.t(:unauthorized)
+      redirect_back_or_default('/index')
+      return
+    end
+  end
 end
--- app/models/cup.rb
+++ app/models/cup.rb
@@ -1,2 +1,185 @@
 class Cup < ActiveRecord::Base
+
+  acts_as_solr :fields => [:name, :time_zone] if use_solr? #, :include => [:sport, :marker] 
+                  
+  has_attached_file :photo,
+  :styles => {
+    :thumb  => "80x80#",
+    :medium => "160x160>",
+    },
+    :storage => :s3,
+    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+    :url => "/assets/cups/:id/:style.:extension",
+    :path => ":assets/cups/:id/:style.:extension",
+    :default_url => "cup_avatar.png"  
+
+    validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/gif', 'image/jpg', 'image/pjpeg']
+    validates_attachment_size         :photo, :less_than => 5.megabytes
+    
+
+  # validations 
+  validates_uniqueness_of   :name,    :case_sensitive => false
+  
+  validates_presence_of     :name
+  validates_presence_of     :description
+  validates_presence_of     :time_zone
+  validates_presence_of     :sport_id
+  
+  validates_length_of       :name,            :within => NAME_RANGE_LENGTH
+  validates_length_of       :description,     :within => DESCRIPTION_RANGE_LENGTH
+      
+  validates_format_of       :name,            :with => /^[A-z 0-9 _.-]*$/ 
+  validates_format_of       :second_team,     :with => /^[A-z 0-9 _.-]*$/ 
+    
+  validates_numericality_of :points_for_win,  :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100
+  validates_numericality_of :points_for_lose, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100
+  validates_numericality_of :points_for_draw, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100
+
+  # variables to access
+  attr_accessible :name, :gameday_at, :points_for_win, :points_for_draw, :points_for_lose
+  attr_accessible :time_zone, :sport_id, :description, :photo
+    
+  # friendly url and removes id  
+  has_friendly_id :name, :use_slug => true, :reserved => ["new", "create", "index", "list", "signup", "edit", "update", "destroy", "show"]
+                  
+  has_and_belongs_to_many :users,           :join_table => "cups_users",   :order => "name"
+
+  has_many      :classifieds
+  has_many      :schedules
+  has_many      :addresses  
+  has_many      :accounts 
+  has_many      :messages
+  has_many      :accounts  
+  has_many      :payments
+  has_many      :scorecards, :conditions => "user_id > 0 and played > 0 and archive = false", :order => "points DESC, ranking"
+  
+  has_many      :archive_scorecards, 
+                :through => :scorecards,
+                :conditions => ["user_id > 0 and played > 0 and season_ends_at < ?", Time.zone.now], 
+                :order => "points DESC, ranking"
+                
+  has_many      :fees   
+
+  has_many :the_managers,
+  :through => :manager_roles,
+  :source => :roles_users
+
+  has_many  :manager_roles,
+  :class_name => "Role", 
+  :foreign_key => "authorizable_id", 
+  :conditions => ["roles.name = 'manager' and roles.authorizable_type = 'Group'"]
+  
+  has_many :the_subscriptions,
+  :through => :subscription_roles,
+  :source => :roles_users
+
+  has_many  :subscription_roles,
+  :class_name => "Role", 
+  :foreign_key => "authorizable_id", 
+  :conditions => ["roles.name = 'subscription' and roles.authorizable_type = 'Group'"]
+
+  belongs_to    :sport   
+  belongs_to    :marker 
+
+  has_one       :blog
+
+  before_create :format_description, :format_conditions
+  before_update :format_description, :format_conditions
+  after_create  :create_cup_blog_details, :create_cup_marker, :create_cup_scorecard
+
+  # # method section
+    
+  def object_counter(objects)
+    @counter = 0
+    objects.each { |object|  @counter += 1 }
+    return @counter
+  end
+
+  def all_the_managers
+    ids = []
+    self.the_managers.each {|user| ids << user.user_id }
+    the_users = User.find(:all, :conditions => ["id in (?)", ids], :order => "name")
+  end
+
+  def avatar
+    self.photo.url
+  end
+
+  def thumbnail
+    self.photo.url
+  end
+
+  def icon
+    self.photo.url
+  end
+  
+  def has_schedule?
+    self.schedules.count > 0
+  end
+  
+  def is_futbol?
+   	# sports related to goals
+    return [1, 2, 3, 4, 5].include?(self.sport_id)
+  end
+  
+  def is_basket?
+   	# sports related to basket
+    return [7].include?(self.sport_id)
+  end
+
+  def self.looking_for_user(user)
+    find(:all, 
+    :conditions => ["id not in (?) and archive = false and looking = true and time_zone = ?", user.cups, user.time_zone],
+    :order => "updated_at",
+     :limit => LOOKING_USERS) 
+  end
+  
+  def available_users
+      self.users.find(:all, :conditions => 'available = true', :order => 'users.name')      
+  end
+  
+  def games_played
+    games_played = 0
+    self.schedules.each {|schedule| games_played += 1 if schedule.played}
+
+    # @match = Match.find(:first, :select => "count(*) as total", 
+    # :conditions => ["schedule_id in (select id from schedules where cup_id = ?) and type_id = 1", self.id],
+    # :cup => "user_id", :order => "count(*) desc")
+    # 
+    # games_played = @match.total
+    return games_played
+  end
+
+  def create_cup_details(user)
+    user.has_role!(:manager, self)
+    user.has_role!(:creator, self)
+    user.has_role!(:member,  self)
+
+    Scorecard.create_user_scorecard(user, self)    
+    GroupsUsers.join_team(user, self)
+  end
+  
+  def format_description
+    self.description.gsub!(/\r?\n/, "<br>") unless self.description.nil?
+  end
+  
+  def format_conditions
+    self.conditions.gsub!(/\r?\n/, "<br>") unless self.conditions.nil?
+  end
+
+private
+  def create_cup_marker
+    GroupsMarkers.join_marker(self, self.marker)
+  end
+
+  def create_cup_blog_details
+    @blog = Blog.create_cup_blog(self)
+    # @entry = Entry.create_cup_entry(self, @blog)
+    # Comment.create_cup_comment(self, @blog, @entry)
+  end
+
+  def create_cup_scorecard   
+    Scorecard.create_cup_scorecard(self)
+  end
 end
+
