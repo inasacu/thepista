671147fba5c8e373c84bb970abd4921c94156b01
Raúl Padilla <inasacu@gmail.com>
2010-05-11 00:06:54 +0200

    removed first game and subsequent game models...not needed

--- app/models/first_game.rb
+++
@@ -1,4 +0,0 @@
-class FirstGame < Game
-  belongs_to :home,   :class_name => 'Escuadra',  :foreign_key => 'home_id' 
-  belongs_to :away,   :class_name => 'Escuadra',  :foreign_key => 'away_id'
-end
