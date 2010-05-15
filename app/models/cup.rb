c47f92b2161e6db63ed2f1339615b395b6ac001a
Raúl Padilla <inasacu@gmail.com>
2010-05-15 23:29:37 +0200

    remove scorecard if none available

--- app/models/cup.rb
+++ app/models/cup.rb
@@ -21,13 +21,13 @@ class Cup < ActiveRecord::Base
   
   validates_presence_of     :name
   validates_presence_of     :description
-  validates_presence_of     :conditions
+  # validates_presence_of     :conditions
   validates_presence_of     :time_zone
   validates_presence_of     :sport_id
   
   validates_length_of       :name,            :within => NAME_RANGE_LENGTH
   validates_length_of       :description,     :within => DESCRIPTION_RANGE_LENGTH
-  validates_length_of       :conditions,      :within => DESCRIPTION_RANGE_LENGTH
+  # validates_length_of       :conditions,      :within => DESCRIPTION_RANGE_LENGTH
       
   validates_format_of       :name,            :with => /^[A-z 0-9 _.-]*$/ 
     
