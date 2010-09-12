4b2c8166d8558a4c0c86c88a4b555b69fb1f6562
Raúl Padilla <inasacu@gmail.com>
2010-09-12 23:05:08 +0200

    set environment to reflect latest friendly id gem

--- config/environment.rb
+++ config/environment.rb
@@ -35,7 +35,8 @@ Rails::Initializer.run do |config|
   config.gem "acl9", :source => "http://gemcutter.org", :lib => "acl9"
   config.gem "geokit"
   config.gem "contacts"  
-  config.gem "friendly_id", :version => "= 2.2.4"  
+  # config.gem "friendly_id", :version => "= 2.2.4"  
+  config.gem "friendly_id"
   config.gem 'sitemap_generator', :lib => false, :source => 'http://gemcutter.org'
   config.gem "edgarjs-ajaxful_rating", :lib => "ajaxful_rating", :source => "http://gems.github.com"
   config.gem 'jackdempsey-acts_as_commentable', :lib => 'acts_as_commentable', :source => "http://gems.github.com"
