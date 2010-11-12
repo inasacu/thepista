1677f5ad93b1e9af5f9229c66d9c2ead018b2732
Raúl Padilla <inasacu@gmail.com>
2010-11-12 23:30:49 +0100

    still trying to get unique values in recientes actividades 2

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -205,7 +205,7 @@ class Schedule < ActiveRecord::Base
   end
   
   def self.latest_matches(items)
-    find(:all, :select => "distinct schedules.id, schedules.concept, schedules.group_id, schedules.played, schedules.updated_at as created_at", 
+    find(:all, :select => "schedules.id, schedules.concept, schedules.group_id, schedules.played, schedules.updated_at as created_at", 
          :joins => "left join matches on matches.schedule_id = schedules.id",
          :conditions => ["matches.updated_at >= ? and matches.archive = false and group_score is not null and invite_score is not null", LAST_24_HOURS]).each do |item| 
       items << item
