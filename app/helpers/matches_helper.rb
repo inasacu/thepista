6e4f93638b8ace3e47854020de2bb5bc42203d2a
Raúl Padilla <inasacu@gmail.com>
2010-11-19 00:16:34 +0100

    reduce team roster convocatoria images to 16x16 and column size to keep all icons on same line

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -15,7 +15,7 @@ module MatchesHelper
     when 4
       the_image = 'status_offline.png'
     end      
-    link_to(image_tag(the_image, :title => the_label, :size => '22x22'), match_status_path(:id => match.id, :type => type.id), :title => the_label)      
+    link_to(image_tag(the_image, :title => the_label, :style => "height: 16px; width: 16px;"), match_status_path(:id => match.id, :type => type.id), :title => the_label)      
   end
 
   def match_roster_link(text, match = nil, html_options = nil)
