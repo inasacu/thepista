ad9c6f8860b6d5785913f111d0c5a1ed91f779dc
Raúl Padilla <inasacu@gmail.com>
2010-12-10 00:23:50 +0100

    removed new fields to not commit error in rake db:migrate in production

--- db/migrate/20101209174759_add_user_company_inside_outside.rb
+++ db/migrate/20101209174759_add_user_company_inside_outside.rb
@@ -1,15 +1,15 @@
 class AddUserCompanyInsideOutside < ActiveRecord::Migration
   def self.up
-    add_column        :users,       :company_url,         :string
-    add_column        :users,       :company_inside,      :boolean,     :default => false
-    add_column        :users,       :company_outside,     :boolean,     :default => false
-    # add_column        :users,       :company_starts_at,   :datetime
+    # add_column        :users,       :company_url,         :string
+    # add_column        :users,       :company_inside,      :boolean,     :default => false
+    # add_column        :users,       :company_outside,     :boolean,     :default => false
+    # # add_column        :users,       :company_starts_at,   :datetime
   end
 
   def self.down
-    remove_column     :users,       :company_url
-    remove_column     :users,       :company_inside
-    remove_column     :users,       :company_outside
-    # remove_column     :users,       :company_starts_at
+    # remove_column     :users,       :company_url
+    # remove_column     :users,       :company_inside
+    # remove_column     :users,       :company_outside
+    # # remove_column     :users,       :company_starts_at
   end
 end
