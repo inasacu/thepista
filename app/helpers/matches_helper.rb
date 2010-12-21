178790da4e169fd2f6f82a628d68433838eb9f54
Raúl Padilla <inasacu@gmail.com>
2010-12-21 22:41:06 +0100

    changed icons...corrected team roster box icons

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -2,18 +2,18 @@ module MatchesHelper
 
   def match_roster_change_link(match, type)    
     the_schedule = match.schedule
-    the_image = 'status_online.png'
+    the_image = 'estatus-convocado.png'
     the_label = the_label = "#{I18n.t(:change_roster_status) } #{I18n.t(type.name)}"
 
     case type.id
     when 1
-      the_image = 'status_online.png'
+      the_image = 'estatus-convocado.png'
     when 2
-      the_image = 'status_away.png' 
+      the_image = 'estatus-ultima-hora.png' 
     when 3
-      the_image = 'status_busy.png' 
+      the_image = 'estatus-ausente.png' 
     when 4
-      the_image = 'status_offline.png'
+      the_image = 'estatus-no-disponible.png'
     end      
     link_to(image_tag(the_image, :title => the_label, :style => "height: 16px; width: 16px;"), match_status_path(:id => match.id, :type => type.id), :title => the_label)      
   end
@@ -48,16 +48,16 @@ module MatchesHelper
 
     case match.type_id
     when 1
-      the_image = 'status_online.png' if the_image.blank?
+      the_image = 'estatus-convocado.png' if the_image.blank?
       return link_to(image_tag(the_image, :title => the_label, :style => "height: 16px; width: 16px;"), team_roster_path(:id => the_schedule), :title => the_label)
     when 2
-      the_image = 'status_away.png' if the_image.blank?
+      the_image = 'estatus-ultima-hora.png' if the_image.blank?
       return link_to(image_tag(the_image, :title => the_label, :style => "height: 15px; width: 15px;"), team_last_minute_path(:id => the_schedule), :title => the_label)
     when 3
-      the_image = 'status_busy.png' if the_image.blank?
+      the_image = 'estatus-ausente.png' if the_image.blank?
       return link_to(image_tag(the_image, :title => the_label, :style => "height: 15px; width: 15px;"), team_no_show_path(:id => the_schedule), :title => the_label)
     when 4
-      the_image = 'status_offline.png' if the_image.blank?
+      the_image = 'estatus-no-disponible.png' if the_image.blank?
       return link_to(image_tag(the_image, :title => the_label, :style => "height: 15px; width: 15px;"), team_unavailable_path(:id => the_schedule), :title => the_label)
     end    
   end
---
+++ information/database_updates.sql
@@ -0,0 +1,5 @@
+
+
+
+# add self to manage any team
+insert into roles_users (role_id, user_id) values (72573, 2001)
