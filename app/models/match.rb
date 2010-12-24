97bafc1f007fcd6776fcec84ac252234375d6cc1
Raúl Padilla <inasacu@gmail.com>
2010-12-24 17:13:31 +0100

    added option to latest items where a new event will now show all users set to ausente...

--- app/models/match.rb
+++ app/models/match.rb
@@ -36,7 +36,7 @@ class Match < ActiveRecord::Base
   def self.latest_items(items, user)
     find(:all, :select => "distinct matches.id, matches.user_id, matches.schedule_id, matches.type_id, types.name as type_name, matches.status_at as created_at", 
          :joins => "left join groups_users on groups_users.user_id = matches.user_id left join types on types.id = matches.type_id",    
-         :conditions => ["groups_users.group_id in (?) and matches.status_at != matches.created_at and matches.status_at >= ?", user.groups, LAST_WEEK]).each do |item| 
+         :conditions => ["groups_users.group_id in (?) and age(matches.status_at, matches.created_at) > '00:00:00' and matches.status_at != matches.created_at and matches.status_at >= ?", user.groups, LAST_WEEK]).each do |item| 
       items << item
     end
     return items 
