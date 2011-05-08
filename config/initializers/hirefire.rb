30b06f3ee028eee5e7442593ea62b89613d66fa0
Raúl Padilla <inasacu@gmail.com>
2011-05-08 21:16:28 +0200

    set hirefire on initiatizer

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,13 +1,13 @@
-# require 'delayed_job'
-# require 'hirefire'
-# # HireFire::Initializer.initialize!
-# 
-# HireFire.configure do |config|
-#   config.max_workers = 5
-#   config.job_worker_ratio = [
-#     { :when => lambda {|jobs| jobs < 15 }, :workers => 1 },
-#     { :when => lambda {|jobs| jobs < 35 }, :workers => 2 },
-#     { :when => lambda {|jobs| jobs < 60 }, :workers => 3 },
-#     { :when => lambda {|jobs| jobs < 80 }, :workers => 4 }
-#   ]
-# end
+require 'delayed_job'
+require 'hirefire'
+# HireFire::Initializer.initialize!
+
+HireFire.configure do |config|
+  config.max_workers = 5
+  config.job_worker_ratio = [
+    { :when => lambda {|jobs| jobs < 15 }, :workers => 1 },
+    { :when => lambda {|jobs| jobs < 35 }, :workers => 2 },
+    { :when => lambda {|jobs| jobs < 60 }, :workers => 3 },
+    { :when => lambda {|jobs| jobs < 80 }, :workers => 4 }
+  ]
+end
