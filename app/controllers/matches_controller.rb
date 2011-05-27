75663a521ee99d009817fc01a39452b4ced1a161
Raúl Padilla <inasacu@gmail.com>
2011-05-27 20:38:20 +0200

    if match status change w/n 24 hours...send to leave a comment on muro

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -102,14 +102,24 @@ class MatchesController < ApplicationController
     @type = Type.find(params[:type])
     played = (@type.id == 1 and !@match.group_score.nil? and !@match.invite_score.nil?)
 
+    ask_for_comment = (current_user == @match.user and Time.zone.now + 1.days > @match.schedule.starts_at)
+    
     if @match.update_attributes(:type_id => @type.id, :played => played, :user_x_two => @user_x_two, :status_at => Time.zone.now)
-      # Scorecard.calculate_user_played_assigned_scorecard(@match.user, @match.schedule.group)
       Scorecard.delay.calculate_user_played_assigned_scorecard(@match.user, @match.schedule.group)
       
       # set fee type_id to same as match type_id
       the_fee = Fee.find(:all, :conditions => ["debit_type = 'User' and debit_id = ? and item_type = 'Schedule' and item_id = ?", @match.user_id, @match.schedule_id])
       the_fee.each {|fee| fee.type_id = @type.id; fee.save}
     end 
+    
+    if ask_for_comment
+      @schedule = @match.schedule
+      @the_previous = Schedule.previous(@schedule)
+      @the_next = Schedule.next(@schedule)
+      @forum = @schedule.forum
+      render :template => 'matches/set_status'
+      return
+    end
     redirect_back_or_default('/index')
   end
 
