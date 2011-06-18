8e3d0432f2236b91549950d47c06ae09c050a211
Raúl Padilla <inasacu@gmail.com>
2011-06-18 21:55:50 +0200

    removed any code related to manipulating the comment table for after season

--- db/migrate/20110521170400_remove_comment_table_fields.rb
+++ db/migrate/20110521170400_remove_comment_table_fields.rb
@@ -1,24 +1,9 @@
 class RemoveCommentTableFields < ActiveRecord::Migration
-  def self.up
-    remove_column     :comments,      :entry_id
-    remove_column     :comments,      :group_id
-    
-    add_column        :comments,      :parent_id,     :integer
-    # add_column        :comments,      :lft,         :integer
-    # add_column        :comments,      :rgt,         :integer
-    
-    add_column        :matches,       :block_token,   :string
-  
+  def self.up    
+    add_column        :matches,       :block_token,   :string  
   end
 
-  def self.down
-    add_column        :comments,      :entry_id,    :integer
-    add_column        :comments,      :group_id,    :integer
-    
-    remove_column     :comments,      :parent_id
-    # remove_column     :comments,      :lft
-    # remove_column     :comments,      :rgt
-    
+  def self.down    
     remove_column     :matches,       :block_token
   end
 end
