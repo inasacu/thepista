0ae4674172301051478508b8445431a4e3501c45
Raul Padilla <inasacu@gmail.com>
2012-03-21 01:24:53 +0100

    adding options for rpx now api for each environment

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Thepista::Application.initialize!
 
+# rpx_now 
+RPXNow.api_key = ENV['RPX_API_KEY']
+RPX_APP_NAME = ENV['RPX_APP_NAME']
 
-RPXNow.api_key = "f7bbfeed95a9e71fe994397d496614da8972aa2c"
+raise "RPX/Janrain Engage API key must be defined ENV['RPX_API_KEY']" unless RPXNow.api_key
+raise "RPX/Janrain Engage Application Name must be defined ENV['RPX_APP_NAME']" unless RPX_APP_NAME
+
+
 WillPaginate.per_page = 10
---
+++ config/initializers/rpx_now.rb
@@ -0,0 +1,24 @@
+
+# thepista.local
+if Rails.env.development?
+	ENV['RPX_API_KEY'] = 'f7bbfeed95a9e71fe994397d496614da8972aa2c'
+	ENV['RPX_APP_NAME'] = 'thepista-local'
+end
+
+# thepista.heroku.com
+if Rails.env.test?
+	ENV['RPX_API_KEY'] = "59b7bd4a615a2c17439b6a4ca064a1d5f482944e" 
+	ENV['RPX_APP_NAME'] = "thepista-heroku"  
+end
+
+# thepista.heroku.com
+if Rails.env.staging?
+	ENV['RPX_API_KEY'] = "59b7bd4a615a2c17439b6a4ca064a1d5f482944e" 
+	ENV['RPX_APP_NAME'] = "thepista-heroku"
+end
+
+# haypista.com
+if Rails.env.production?
+	ENV['RPX_API_KEY'] = "2acf2c6b39e3d1d988629e0cb41c675827c2f45e"   
+	ENV['RPX_APP_NAME'] = "haypista"  
+end
