0e7ad37b4a118f9600be08922d5ca5ffabe1287b
Raul Padilla <inasacu@gmail.com>
2012-08-05 13:30:55 +0200

    continue issues w/ migration to staging...forum table

--- db/migrate/20090830181157_add_forum_practice_field.rb
+++ db/migrate/20090830181157_add_forum_practice_field.rb
@@ -1,9 +1,6 @@
 class AddForumPracticeField < ActiveRecord::Migration
   def self.up
-    
-     # rake db:migrate VERSION=20090828013436
-    
-      add_column      :forums,    :practice_id,     :integer
+      # add_column      :forums,    :practice_id,     :integer
       change_column   :users,     :language,        :string,    :default => 'es'
   end
 
