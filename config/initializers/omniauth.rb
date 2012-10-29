72be85b083bdd2150df973c1b8024a50ff539043
Raul Padilla <inasacu@gmail.com>
2012-10-29 21:01:49 +0100

    added facebook key and info to phase-out janrain login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -8,14 +8,17 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 		provider 	:facebook,		'503725422971581', '93b9ffce01692d855a756bc51619f2d2' 
 		provider 	:linkedin,		'6uezehrhpsxg', 'PX4ePvBeZ17O2Twc' 
 	end
+	
+	# staging
+	# provider 	:facebook,		'554736744543441', '7382d6db12392eea9fc3ed026e70b3ca'
 
 	unless Rails.env.development?
-		provider 	:facebook,		'64553101275', 'e6425a5b426fa550a57b027e4ffe2dd4'				
+		provider 	:facebook,		'273731636077992', '9f7013153c332f7d6b666814b8f6c7f0'				
 		provider 	:linkedin,		'gb7lo5u6pmse', '9SYISnZCVF9nUd28'
 	end
 
 end
-
+	
 
 # This is a list of the strategies that are available for OmniAuth version 1.0 and later. 
 # https://github.com/intridea/omniauth/wiki/List-of-Strategies
