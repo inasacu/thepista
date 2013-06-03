d15a52dfc0933876ac7b483f04200408313fdaef
Jonathan <jonathan.aradu@gmail.com>
2013-06-03 15:57:16 +0200

    Branch validation

--- app/controllers/widget_controller.rb
+++ app/controllers/widget_controller.rb
@@ -7,7 +7,10 @@ class WidgetController < ApplicationController
   def home
     
     currentBranch = Branch.branch_from_url(request.env["HTTP_REFERER"]) 
-    @schedulesPerWeekDay = Timetable.week_schedules_from_timetables(currentBranch)
+    
+    if !currentBranch.nil?
+      @schedulesPerWeekDay = Timetable.week_schedules_from_timetables(currentBranch)
+    end
         
     render :layout => 'widget'
   end
