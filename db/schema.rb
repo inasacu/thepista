0090513d1618c2d85799482c8e384002b469bd05
Raul Padilla <inasacu@gmail.com>
2013-06-13 00:35:26 +0200

    removed all subplug and enchufado files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130521172705) do
+ActiveRecord::Schema.define(:version => 20130612223236) do
 
   create_table "announcements", :force => true do |t|
     t.text     "message"
