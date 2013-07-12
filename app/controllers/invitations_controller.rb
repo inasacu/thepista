a3404657b6cf6604e576c824535424d87a8ae5af
Raul Padilla <inasacu@gmail.com>
2013-07-12 19:56:11 +0200

    removed message from invitation and set default message...

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,79 +1,74 @@
 class InvitationsController < ApplicationController
-  before_filter :require_user
-  
-  def index  
-    redirect_to :action => 'new'
-  end
-  
-  def new
-    @invitation = Invitation.new
+	before_filter :require_user
 
-    if (params[:group_id])
-      @group = Group.find(params[:group_id])
-    elsif (params[:schedule_id])
-      @schedule = Schedule.find(params[:schedule_id])
-    elsif (params[:challenge_id])
-      @challenge = Challenge.find(params[:challenge_id])
-    elsif (params[:cup_id])
-      @cup = Cup.find(params[:cup_id])
-    end
+	def index  
+		redirect_to :action => 'new'
+	end
 
-    render @the_template  
-  end
-  
-  def create
-    @user = current_user
-    @invitation = Invitation.new(params[:invitation])
-    @invitation.user = @user
+	def new
+		@invitation = Invitation.new
 
-    unless params[:schedule][:id].blank? 
-      @schedule = Schedule.find(params[:schedule][:id])
-      @invitation.item = @schedule
-    end
+		if (params[:group_id])
+			@group = Group.find(params[:group_id])
+		elsif (params[:schedule_id])
+			@schedule = Schedule.find(params[:schedule_id])
+		elsif (params[:challenge_id])
+			@challenge = Challenge.find(params[:challenge_id])
+		elsif (params[:cup_id])
+			@cup = Cup.find(params[:cup_id])
+		end
 
-    unless params[:group][:id].blank?
-      @group = Group.find(params[:group][:id])
-      @invitation.item = @group
-    end
+		render @the_template  
+	end
 
-    unless params[:challenge][:id].blank?
-      @challenge = Challenge.find(params[:challenge][:id])
-      @invitation.item = @challenge
-    end
-    
-    unless params[:cup][:id].blank?
-      @cup = Cup.find(params[:cup][:id])
-      @invitation.item = @cup
-    end
-    
-    email_addresses = @invitation.email_addresses || ''
-    
-    text = "#{text.to_s.strip[0..12]}..." if text.to_s.length > 14
-    
-    @invitation.email_addresses = @invitation.email_addresses.to_s.strip[0..254] if @invitation.email_addresses.to_s.length > 254
+	def create
+		@user = current_user
+		@invitation = Invitation.new(params[:invitation])
+		@invitation.user = @user
 
-    if @invitation.save     
-      
-      # email_addresses = @invitation.email_addresses || ''
-      emails = email_addresses.gsub(",", " ").split(" ").collect{|email| email.strip }.uniq
-      emails.each{ |email|
-        @the_invitation = Invitation.new
-        @the_invitation.message = @invitation.message
-        @the_invitation.item = @invitation.item
-        @the_invitation.user = @invitation.user
-        @the_invitation.email_addresses = email
-        @the_invitation.save!
-      }  
-      # @invitation.destroy  
-    
-      flash[:notice] = I18n.t(:invitation_successful_create)
-    end
+		unless params[:schedule][:id].blank? 
+			@schedule = Schedule.find(params[:schedule][:id])
+			@invitation.item = @schedule
+		end
 
-    redirect_to :action => 'new' 
+		unless params[:group][:id].blank?
+			@group = Group.find(params[:group][:id])
+			@invitation.item = @group
+		end
 
-    # end
-    # redirect_to invitations_url
-  end  
+		unless params[:challenge][:id].blank?
+			@challenge = Challenge.find(params[:challenge][:id])
+			@invitation.item = @challenge
+		end
+
+		unless params[:cup][:id].blank?
+			@cup = Cup.find(params[:cup][:id])
+			@invitation.item = @cup
+		end
+
+		email_addresses = @invitation.email_addresses || ''
+		text = "#{text.to_s.strip[0..12]}..." if text.to_s.length > 14
+		@invitation.email_addresses = @invitation.email_addresses.to_s.strip[0..254] if @invitation.email_addresses.to_s.length > 254
+
+		@invitation.message = I18n.t(:join)
+		
+		emails = email_addresses.gsub(",", " ").split(" ").collect{|email| email.strip }.uniq
+		emails.each{ |email|
+			@the_invitation = Invitation.new
+			@the_invitation.message = @invitation.message
+			@the_invitation.item = @invitation.item
+			@the_invitation.user = @invitation.user
+			@the_invitation.email_addresses = email
+			@the_invitation.save!
+		}  
+
+		flash[:notice] = I18n.t(:invitation_successful_create)
+
+		redirect_to :action => 'new' 
+
+		# end
+		# redirect_to invitations_url
+	end  
 
 end
 
