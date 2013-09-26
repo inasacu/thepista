f2b0d0a11e86a9fbbee74849c1190478d04f249c
Jonathan <jaraujod@me.com>
2013-09-26 08:57:34 +0200

    precompile widget assets

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,8 @@ module Thepista
 
     # Enable the asset pipeline
     config.assets.enabled = true    
+    
+    config.assets.precompile += ['widget/*.css']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
