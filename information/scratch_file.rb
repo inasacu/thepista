8e187f2d61cb617e730055c3582e78fad936fd57
Raul Padilla <inasacu@gmail.com>
2013-11-04 18:32:11 +0100

    precompiled files on staging causing site to crash

--- information/scratch_file.rb
+++ information/scratch_file.rb
@@ -1,5 +1,22 @@
 "http://localhost:8888/thepista_client"
 
+
+       (See full trace by running task with --trace)
+ !
+ !     Precompiling assets failed.
+ !
+
+ !     Push rejected, failed to compile Ruby app
+
+To git@heroku.com:thepista.git
+ ! [remote rejected] master -> master (pre-receive hook declined)
+error: failed to push some refs to 'git@heroku.com:thepista.git'
+Rauls-MacBook-Pro:thepista padilla$ heroku labs:enable user-env-compile -a thepista
+Enabling user-env-compile for thepista... done
+WARNING: This feature is experimental and may change or be removed without notice.
+For more information see: http://devcenter.heroku.com/articles/labs-user-env-compile
+Rauls-MacBook-Pro:thepista padilla$
+
 DUPLICATE ACCOUNTS IN SCORECARD
 
 select id, group_id, user_id, ranking, played, archive
