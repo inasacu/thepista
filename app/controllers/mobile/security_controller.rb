c047894044fe5f02bd811d356a16b8f69bae164d
Jonathan Araujo <jaraujod@me.com>
2014-01-09 20:46:03 +0100

    mobile_response method to add abstraction level

--- app/controllers/mobile/security_controller.rb
+++ app/controllers/mobile/security_controller.rb
@@ -26,24 +26,34 @@ class Mobile::SecurityController < ActionController::Base
     
     if Mobile::MobileToken.check_token(token)
     else
-      security_error_response
+      security_error_response("Security error checking auth token")
     end
   end
   
-  def success_response(response_message=nil)
+  def mobile_response(response_code=nil, response_message=nil)
     response = Mobile::MobileResponse.new
-    response.code = "00"
+    response.code = response_code
     response.message = response_message
     render json: response
     return
   end
   
+  def success_response(response_message=nil)
+    mobile_response("00", response_message)
+  end
+  
+  def error_response(response_message=nil)
+    if response_message.nil?
+      response_message = "Error while handling operation"
+    end
+    mobile_response("88", response_message)
+  end
+  
   def security_error_response(response_message=nil)
-    response = Mobile::MobileResponse.new
-    response.code = "99"
-    response.message = response_message
-    render json: response
-    return
+    if response_message.nil?
+      response_message = "Security error on mobile request"
+    end
+    mobile_response("99", response_message)
   end
   
 end
