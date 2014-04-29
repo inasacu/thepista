851fae23091f173e70f668d74d1949deba323084
Raul Padilla <inasacu@gmail.com>
2014-04-29 23:49:32 +0200

    removed migration files...

--- db/migrate/20140429190518_resetschedulereminder.rb
+++
@@ -1,9 +0,0 @@
-class Resetschedulereminder < ActiveRecord::Migration
-  def up
-    change_column :schedules, :reminder, :boolean, :default => false 
-    # update schedules set reminder = false
-  end
-
-  def down
-  end
-end
